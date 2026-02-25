39809dd955f5728b6f7cf1b6cd8866bfe9d90b4f
bonfim031 <joao.kanherk@escola.pr.gov.br>
2026-02-25 10:45:10 -0300

    Add files via upload
    o que eu sei basico html

---
+++ joao/aula1.js
@@ -0,0 +1,44 @@
+console.log("ola humanos ");
+var a = 10
+var b = 0
+console.log(a/b)
+
+var a = 10
+var b = 5
+console.log(a*b)
+
+var a = 10
+var b = 5
+console.log(a-b)
+
+var a = 10
+var b = 5
+console.log(a+b)
+
+var a = 10
+var b = 5
+console.log(a/b)
+
+var a = 0
+var b = 0
+console.log(a/b)
+
+const cidade = "belo horizonte";
+//const input = "Belo Horizonte";
+//const inputMin = input.toLowerCase()
+//console.log(cidade === inputMin)
+const senha = "minhasenha123"
+console.log(senha.length);
+
+let minhav = null;
+if (minhav === null){
+    console.log('não');
+} else {
+    console.log(minhav);
+}
+
+let input1 = null;
+let input2;
+
+console.log (input1);
+console.log (input2);
